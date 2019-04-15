da4a9b47ab501087e368cd07aeea7c320ca26e17
Rabel Obispo <rabelobispo@hotmail.com>
2019-04-15 19:42:55 -0400

    fixing error with suspcrition

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
 import { RecipeService } from 'src/app/Services/recipe.service';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-recipe-list',
@@ -10,18 +11,21 @@ import { RecipeService } from 'src/app/Services/recipe.service';
 })
 export class RecipeListComponent implements OnInit, OnDestroy {
   recipes: Recipe[];
+  recSuspcription: Subscription;
 
   constructor(private recService: RecipeService) {}
 
   ngOnInit() {
     this.recipes = this.recService.getRecipes();
 
-    this.recService.recipesChange.subscribe((recipes: Recipe[]) => {
-      this.recipes = recipes;
-    });
+    this.recSuspcription = this.recService.recipesChange.subscribe(
+      (recipes: Recipe[]) => {
+        this.recipes = recipes;
+      }
+    );
   }
 
   ngOnDestroy() {
-    this.recService.recipesChange.unsubscribe();
+    this.recSuspcription.unsubscribe();
   }
 }
