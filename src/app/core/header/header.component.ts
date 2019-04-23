3a1b962ae517d78ae0e3506688c4e4672d229453
Rabel Obispo <rabelobispo@hotmail.com>
2019-04-23 15:37:02 -0400

    Moving auth method to ts

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -34,4 +34,8 @@ export class HeaderComponent {
     this.authService.logout();
     this.router.navigate(['/']);
   }
+
+  isAuthenticated() {
+    return this.authService.isAuthenticated();
+  }
 }
